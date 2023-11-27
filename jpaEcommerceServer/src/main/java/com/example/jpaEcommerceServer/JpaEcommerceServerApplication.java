6aa254b72089c4afb2b03edb8c50ce87fe709a0a
AugustoSoria <augustosoria@hotmail.es>
2023-11-27 17:37:50 -0300

    initial commit

---
+++ jpaEcommerceServer/src/main/java/com/example/jpaEcommerceServer/JpaEcommerceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaEcommerceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEcommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEcommerceServerApplication.class, args);
+	}
+
+}
